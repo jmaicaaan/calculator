68836df5926e1f1ee2a82777242159f998f83ef3
jmaicaaan <jmsantosss29@yahoo.com>
2017-08-21 18:25:29 +0800

    Layout fix and some functionalities

--- client/app/components/calculator/calculator.controller.js
+++ client/app/components/calculator/calculator.controller.js
@@ -1,8 +1,25 @@
 class CalculatorController {
   constructor($state, $stateParams) {
     "ngInject";
-    this.name = 'calculator';
+    this.result = 0;
+    this.userInputs = [];
   }
+
+  getResult = () => {
+    this.result += 5;
+  };
+
+  addUserInputs = (char) => {
+    this.userInputs.push(char);
+  };
+  
+  clearUserInputs = (isReset) => {
+    if (isReset) {
+      this.userInputs = [];
+    } else {
+      this.userInputs.pop();
+    }    
+  };
 }
 
 export default CalculatorController;
